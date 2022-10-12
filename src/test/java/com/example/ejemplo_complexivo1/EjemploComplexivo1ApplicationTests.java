17c6fb53e47c0aa37a392c471fc1f68ff880786f
Danny Tenemea <danny.tenemea.est@tecazuay.edu.ec>
2022-10-12 09:29:59 -0500

    Initial commit

---
+++ src/test/java/com/example/ejemplo_complexivo1/EjemploComplexivo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejemplo_complexivo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploComplexivo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
